98482a37a5b0fc4234cb866d554477c73b80e448
hayate <hayate.gotoh@gmail.com>
2015-11-29 08:15:55 +0900

    ねむい

---
+++ module/getTransitApi.js
@@ -0,0 +1,44 @@
+var client = require('cheerio-httpcli');
+var moment = require('moment');
+
+module.exports = function(startStation,endStation,callback){
+  var date = moment().format();
+  var year = moment().format("YYYY");
+  var month = moment().format("MM");
+  var day = moment().format("DD");
+  var hour = moment().format("HH");
+  var minute = moment().format("mm");
+  console.log(minute);
+  var minute1 = minute.substr(0,1);
+  var minute2 = minute.substr(1,1);
+  //var etc = "&type=1&ticket=ic&al=1&shin=1&ex=1&hb=1&lb=1&sr=1&s=0&expkind=1&ws=2"
+  var baseUrl = "http://transit.yahoo.co.jp/search/result";
+  var param = "?flatlon=&from="+encodeURIComponent(startStation)+"&tlatlon=&to="+encodeURIComponent(endStation)+"&via=&via=&via=&y="+year+"&m="+month+"&d="+day+"&hh="+hour+"&m2="+minute2+"&m1="+minute1;
+  var url = baseUrl+param;
+  //url = "http://transit.yahoo.co.jp/search/result?flatlon=&from=%E4%BA%AC%E7%8E%8B%E5%A4%9A%E6%91%A9%E5%B7%9D&tlatlon=&to=%E6%96%B0%E5%AE%BF&via=&via=&via=&y=2015&m=11&d=29&hh=06&m2=1&m1=1&kw=%E6%96%B0%E5%AE%BF";
+  console.log(url);
+  client.fetch(url,function(err,$,res){
+    //console.log($.html());
+    var route = $(".routeDetail");
+    var startStationInfo = route.find(".station").eq(0);
+    var endStationInfo = route.find(".fareSection").eq(0).nextAll(".station").eq(0);
+    var startStationTimeStr = startStationInfo.find(".time li").text();
+    var endStationTimeStr = endStationInfo.find(".time li").text();
+    var lines = [];
+    route.find(".transport").each(function(){
+     if($(this).find(".icon").text()==="[train]"){
+        var lineData = $(this).find("div").text();
+        var line = lineData.match(/\[train\](.*?)・.*/)[1];
+        //console.log(line);
+        lines.push(line);
+      }
+    });
+    var data = {
+      "startTime":"",
+      "endTime" : ""
+    }
+    console.log(startStationTimeStr);
+    console.log(endStationTimeStr);
+  });
+  
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var getTransitApi = require("../module/getTransitApi");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/notify', function(req,res,next){
+  //var postData = req.body;
+  //console.log(postData);
+  //var userId = postData.id;
+  //var checkPoints = postData.checkPoints;
+  getTransitApi("京王多摩川","六本木",function(){});
+});
+
 
 module.exports = router;
